012999bc39b50351d38726948500088e0f1f36d8
HUNKIMY <JJUNEY167@GMAIL.COM>
2023-04-27 05:52:04 -0700

    [level 0] Title: 저주의 숫자 3, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,11 +1,3 @@
 def solution(n):
-    result = 0
-    while n:
-        while True:
-            result += 1
-            if result % 3 ==0 or '3' in str(result):
-                pass
-            else:
-                break
-        n -= 1
-    return result
+    answer = 0
+    return answer
